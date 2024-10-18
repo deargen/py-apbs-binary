456ca72e4c3267cee8ba98ef178c92bdb3aa7b58
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-10-19 02:38:06 +0900

    refactor!: much better typing and API

--- python/src/apbs_binary/executable.py
+++ python/src/apbs_binary/executable.py
@@ -1,71 +1,27 @@
+"""
+Wrapper that does subprocess.run or Popen with given binaries, with the correct environment variables set.
+
+To perfect the type hinting, we have executable.pyi that copies the signatures
+from subprocess.run and subprocess.Popen.
+Normally, functions with *args, **kwargs are hard to type hint.
+"""
+
 from __future__ import annotations
 
 import os
-import platform
 import subprocess
 import sys
+from collections.abc import Callable
 from copy import deepcopy
-from pathlib import Path
-from typing import Any, Literal, overload
+from os import PathLike
 
-BIN_DIR = Path(__file__).parent / "bin"
-# NOTE: lib/ only exists for macos arm64, because we built using homebrew
-# and it didn't build entirely statically.
-# Thus we have to pass DYLD_LIBRARY_PATH to the subprocess.run call.
-LIB_DIR = Path(__file__).parent / "lib"
+from . import LIB_DIR, bin_path
 
 
-def bin_path(bin_name: str) -> Path:
-    if os.name == "nt":
-        return BIN_DIR / f"{bin_name}.exe"
-    return BIN_DIR / bin_name
-
-
-# bin/
-APBS_BIN_PATH = bin_path("apbs")
-
-# originally share/apbs/tools/bin/, but moved to bin/
-ANALYSIS_BIN_PATH = bin_path("analysis")
-BENCHMARK_BIN_PATH = bin_path("benchmark")
-BORN_BIN_PATH = bin_path("born")
-COULOMB_BIN_PATH = bin_path("coulomb")
-DEL2DX_BIN_PATH = bin_path("del2dx")
-DX2MOL_BIN_PATH = bin_path("dx2mol")
-DX2UHBD_BIN_PATH = bin_path("dx2uhbd")
-DXMATH_BIN_PATH = bin_path("dxmath")
-MERGEDX_BIN_PATH = bin_path("mergedx")
-MERGEDX2_BIN_PATH = bin_path("mergedx2")
-MGMESH_BIN_PATH = bin_path("mgmesh")
-MULTIVALUE_BIN_PATH = bin_path("multivalue")
-SIMILARITY_BIN_PATH = bin_path("similarity")
-SMOOTH_BIN_PATH = bin_path("smooth")
-TENSOR2DX_BIN_PATH = bin_path("tensor2dx")
-UHBD_ASC2BIN_BIN_PATH = bin_path("uhbd_asc2bin")
-VALUE_BIN_PATH = bin_path("value")
-
-
-@overload
-def process_run(
-    bin_name, *args: str, return_completed_process: Literal[True], **kwargs: Any
-) -> subprocess.CompletedProcess[str | bytes]: ...
-
-
-@overload
-def process_run(
-    bin_name, *args: str, return_completed_process: Literal[False] = ..., **kwargs: Any
-) -> int: ...
-
-
-@overload
-def process_run(
-    bin_name, *args: str, return_completed_process: bool = False, **kwargs: Any
-) -> int | subprocess.CompletedProcess[str | bytes]: ...
-
-
-def process_run(
-    bin_name, *args: str, return_completed_process: bool = False, **kwargs: Any
-) -> int | subprocess.CompletedProcess[str | bytes]:
-    # if mac arm64, set DYLD_LIBRARY_PATH
+def _subprocess_with_env(run_or_popen_func: Callable, *args, **kwargs):
+    """
+    In macOS, we need to set DYLD_LIBRARY_PATH to the lib/ directory.
+    """
     if sys.platform == "darwin":
         my_env: dict[str, str]
         if kwargs.get("env") is not None:
@@ -75,60 +31,172 @@ def process_run(
         else:
             my_env = os.environ.copy()
             my_env["DYLD_LIBRARY_PATH"] = str(LIB_DIR)
-        complete_process = subprocess.run(
-            [bin_path(bin_name), *args], env=my_env, **kwargs
-        )
-    # elif os.name == "nt":
-    #     # dll files are together with the binaries.
-    #     # so we need to add the directory to PATH
-    #     my_env = os.environ.copy()
-    #     my_env["PATH"] = f"{BIN_DIR!s};{my_env['PATH']}"
-    #     complete_process = subprocess.run(
-    #         [bin_path(bin_name), *args], env=my_env, shell=True, **kwargs
-    #     )
+        return run_or_popen_func(*args, env=my_env, **kwargs)
+
+    return run_or_popen_func(*args, **kwargs)
+
+
+def _subprocess_with_env_and_bin(run_or_popen_func, bin_name: str, *args, **kwargs):
+    if len(args) > 0:
+        cmd_args = args[0]
+        args = args[1:]
     else:
-        complete_process = subprocess.run([bin_path(bin_name), *args], **kwargs)
+        cmd_args = None
 
-    if return_completed_process:
-        return complete_process
-    return complete_process.returncode
-
-
-@overload
-def apbs(
-    *args: str, return_completed_process: Literal[True], **kwargs: Any
-) -> subprocess.CompletedProcess[str | bytes]: ...
-
-
-@overload
-def apbs(
-    *args: str, return_completed_process: Literal[False] = ..., **kwargs: Any
-) -> int: ...
-
-
-def apbs(
-    *args: str, return_completed_process: bool = False, **kwargs: Any
-) -> int | subprocess.CompletedProcess[str | bytes]:
-    return process_run(
-        "apbs", *args, return_completed_process=return_completed_process, **kwargs
+    if cmd_args is None or (isinstance(cmd_args, str) and cmd_args == ""):
+        return _subprocess_with_env(
+            run_or_popen_func, bin_path(bin_name), *args, **kwargs
+        )
+    elif isinstance(cmd_args, (str, bytes, PathLike)):
+        return _subprocess_with_env(
+            run_or_popen_func, [bin_path(bin_name), cmd_args], *args, **kwargs
+        )
+    return _subprocess_with_env(
+        run_or_popen_func, [bin_path(bin_name), *cmd_args], *args, **kwargs
     )
 
 
-@overload
-def multivalue(
-    *args: str, return_completed_process: Literal[True], **kwargs: Any
-) -> subprocess.CompletedProcess[str | bytes]: ...
+def run_apbs(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "apbs", *args, **kwargs)
 
 
-@overload
-def multivalue(
-    *args: str, return_completed_process: Literal[False] = ..., **kwargs: Any
-) -> int: ...
+def popen_apbs(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "apbs", *args, **kwargs)
 
 
-def multivalue(
-    *args: str, return_completed_process: bool = False, **kwargs: Any
-) -> int | subprocess.CompletedProcess[str | bytes]:
-    return process_run(
-        "multivalue", *args, return_completed_process=return_completed_process, **kwargs
+def run_multivalue(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "multivalue", *args, **kwargs)
+
+
+def popen_multivalue(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "multivalue", *args, **kwargs)
+
+
+def run_analysis(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "analysis", *args, **kwargs)
+
+
+def popen_analysis(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "analysis", *args, **kwargs)
+
+
+def run_benchmark(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "benchmark", *args, **kwargs)
+
+
+def popen_benchmark(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "benchmark", *args, **kwargs)
+
+
+def run_born(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "born", *args, **kwargs)
+
+
+def popen_born(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "born", *args, **kwargs)
+
+
+def run_coulomb(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "coulomb", *args, **kwargs)
+
+
+def popen_coulomb(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "coulomb", *args, **kwargs)
+
+
+def run_del2dx(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "del2dx", *args, **kwargs)
+
+
+def popen_del2dx(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "del2dx", *args, **kwargs)
+
+
+def run_dx2mol(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "dx2mol", *args, **kwargs)
+
+
+def popen_dx2mol(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "dx2mol", *args, **kwargs)
+
+
+def run_dx2uhbd(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "dx2uhbd", *args, **kwargs)
+
+
+def popen_dx2uhbd(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "dx2uhbd", *args, **kwargs)
+
+
+def run_dxmath(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "dxmath", *args, **kwargs)
+
+
+def popen_dxmath(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "dxmath", *args, **kwargs)
+
+
+def run_mergedx(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "mergedx", *args, **kwargs)
+
+
+def popen_mergedx(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "mergedx", *args, **kwargs)
+
+
+def run_mergedx2(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "mergedx2", *args, **kwargs)
+
+
+def popen_mergedx2(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "mergedx2", *args, **kwargs)
+
+
+def run_mgmesh(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "mgmesh", *args, **kwargs)
+
+
+def popen_mgmesh(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "mgmesh", *args, **kwargs)
+
+
+def run_similarity(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "similarity", *args, **kwargs)
+
+
+def popen_similarity(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "similarity", *args, **kwargs)
+
+
+def run_smooth(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "smooth", *args, **kwargs)
+
+
+def popen_smooth(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "smooth", *args, **kwargs)
+
+
+def run_tensor2dx(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "tensor2dx", *args, **kwargs)
+
+
+def popen_tensor2dx(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "tensor2dx", *args, **kwargs)
+
+
+def run_uhbd_asc2bin(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "uhbd_asc2bin", *args, **kwargs)
+
+
+def popen_uhbd_asc2bin(*args, **kwargs):
+    return _subprocess_with_env_and_bin(
+        subprocess.Popen, "uhbd_asc2bin", *args, **kwargs
     )
+
+
+def run_value(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.run, "value", *args, **kwargs)
+
+
+def popen_value(*args, **kwargs):
+    return _subprocess_with_env_and_bin(subprocess.Popen, "value", *args, **kwargs)
--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 from collections.abc import Callable
 from pathlib import Path
@@ -6,30 +8,63 @@ import pytest
 from apbs_binary import (
     APBS_BIN_PATH,
     MULTIVALUE_BIN_PATH,
-    apbs,
-    multivalue,
-    process_run,
+    popen_analysis,
+    popen_apbs,
+    popen_benchmark,
+    popen_born,
+    popen_coulomb,
+    popen_del2dx,
+    popen_dx2mol,
+    popen_dx2uhbd,
+    popen_dxmath,
+    popen_mergedx,
+    popen_mergedx2,
+    popen_mgmesh,
+    popen_multivalue,
+    popen_similarity,
+    popen_smooth,
+    popen_tensor2dx,
+    popen_uhbd_asc2bin,
+    popen_value,
+    run_analysis,
+    run_apbs,
+    run_benchmark,
+    run_born,
+    run_coulomb,
+    run_del2dx,
+    run_dx2mol,
+    run_dx2uhbd,
+    run_dxmath,
+    run_mergedx,
+    run_mergedx2,
+    run_mgmesh,
+    run_multivalue,
+    run_similarity,
+    run_smooth,
+    run_tensor2dx,
+    run_uhbd_asc2bin,
+    run_value,
 )
 
 RETURN_CODE_NO_ARGS = [
-    ("apbs", 13),
-    ("multivalue", 1),
-    ("analysis", 2),
-    ("benchmark", 12),
-    ("born", 154),
-    ("coulomb", 154),
-    ("del2dx", 1),
-    ("dx2mol", 1),
-    ("dx2uhbd", 1),
-    ("dxmath", 13),
-    ("mergedx", 255),
-    ("mergedx2", 1),
-    ("mgmesh", 0),
-    ("similarity", 2),
-    ("smooth", 2),
-    ("tensor2dx", 255),
-    ("uhbd_asc2bin", -11),
-    ("value", 2),
+    (run_apbs, popen_apbs, 13),
+    (run_multivalue, popen_multivalue, 1),
+    (run_analysis, popen_analysis, 2),
+    (run_benchmark, popen_benchmark, 12),
+    (run_born, popen_born, 154),
+    (run_coulomb, popen_coulomb, 154),
+    (run_del2dx, popen_del2dx, 1),
+    (run_dx2mol, popen_dx2mol, 1),
+    (run_dx2uhbd, popen_dx2uhbd, 1),
+    (run_dxmath, popen_dxmath, 13),
+    (run_mergedx, popen_mergedx, 255),
+    (run_mergedx2, popen_mergedx2, 1),
+    (run_mgmesh, popen_mgmesh, 0),
+    (run_similarity, popen_similarity, 2),
+    (run_smooth, popen_smooth, 2),
+    (run_tensor2dx, popen_tensor2dx, 255),
+    (run_uhbd_asc2bin, popen_uhbd_asc2bin, -11),
+    (run_value, popen_value, 2),
 ]
 
 
@@ -55,69 +90,90 @@ def test_name(bin_path: Path, expected_name: str):
     assert bin_path.name == expected_name or bin_path.name == expected_name + ".exe"
 
 
-def test_execute_help():
+@pytest.mark.parametrize(
+    ("run_func", "popen_func", "arg", "expected_return_code"),
+    [
+        (run_apbs, popen_apbs, "--help", 13),
+        (run_apbs, popen_apbs, ["--help"], 13),
+    ],
+)
+def test_execute_onearg(
+    run_func: Callable,
+    popen_func: Callable,
+    arg: str | list[str],
+    expected_return_code: int,
+):
     """
+    Test run_apbs and popen_apbs with --help option, str and list arguments.
+
     Note:
         multivalue does not have a --help option.
     """
-    return_code = apbs("--help")
-    assert return_code == 13
+    proc = run_func(arg)
+    assert proc.returncode == expected_return_code
+
+    proc = popen_func(arg)
+    proc.wait()
+    assert proc.returncode == expected_return_code
 
 
 @pytest.mark.parametrize(
-    ("func", "expected_return_code"),
-    [
-        (apbs, 13),
-        (multivalue, 1),
-    ],
-)
-def test_execute_noarg(func: Callable, expected_return_code: int):
-    return_code = func()
-    assert return_code == expected_return_code
-
-
-@pytest.mark.parametrize(
-    ("bin_name", "expected_return_code"),
+    ("run_func", "popen_func", "expected_return_code"),
     RETURN_CODE_NO_ARGS,
 )
-def test_process_run_noarg(bin_name: str, expected_return_code: int):
-    return_code = process_run(bin_name)
-    assert return_code == expected_return_code
+def test_execute_noarg(
+    run_func: Callable, popen_func: Callable, expected_return_code: int
+):
+    proc = run_func("")
+    assert proc.returncode == expected_return_code
+
+    proc = popen_func("")
+    proc.wait()
+    assert proc.returncode == expected_return_code
+
+    # below will fail the type checking but it should still run as expected.
+    proc = run_func()
+    assert proc.returncode == expected_return_code
+
+    proc = popen_func()
+    proc.wait()
+    assert proc.returncode == expected_return_code
 
 
 @pytest.mark.parametrize(
-    ("bin_name", "expected_return_code"),
+    ("run_func", "popen_func", "expected_return_code"),
     RETURN_CODE_NO_ARGS,
 )
-def test_process_run_noarg_with_env(bin_name: str, expected_return_code: int):
+def test_process_run_noarg_with_env(
+    run_func: Callable, popen_func: Callable, expected_return_code: int
+):
     """
     In macOS, the env variable `DYLD_LIBRARY_PATH` is required to run the binaries. Thus, we check if custom env settings still respect this.
     """
     my_env = os.environ.copy()
     my_env["PATH"] = "/usr/local/bin"  # dummy change. Not important
-    return_code = process_run(bin_name, env=my_env)
-    assert return_code == expected_return_code
+    proc = run_func("", env=my_env)
+    assert proc.returncode == expected_return_code
+
+    proc = popen_func("", env=my_env)
+    proc.wait()
+    assert proc.returncode == expected_return_code
 
 
 @pytest.mark.parametrize(
-    ("func", "line_number", "line_match"),
+    ("run_func", "line_number", "line_match"),
     [
-        (apbs, 3, r"APBS -- Adaptive Poisson-Boltzmann Solver"),
+        (run_apbs, 3, r"APBS -- Adaptive Poisson-Boltzmann Solver"),
         (
-            multivalue,
+            run_multivalue,
             2,
             r"Usage: multivalue <csvCoordinatesFile> <dxFormattedFile> <outputFile> [outputformat]",
         ),
     ],
 )
 def test_execute_noarg_message_stdout(
-    func: Callable, line_number: int, line_match: str
+    run_func: Callable, line_number: int, line_match: str
 ):
-    proc = func(return_completed_process=True, capture_output=True, text=True)
-    print(proc)
-    if isinstance(proc.stdout, bytes):
-        lines = proc.stdout.decode("utf-8")
-    else:
-        lines = proc.stdout
-    line = lines.splitlines()[line_number]
+    proc = run_func("", capture_output=True, text=True, encoding="utf-8")
+    line = proc.stdout.splitlines()[line_number]
     assert line.strip() == line_match
